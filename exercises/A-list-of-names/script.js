3cdb500c4fd8dc5e8095b6b35061267c030bc6b5
Simba Pfaira <spfaira@yahoo.com>
2021-03-04 20:29:39 +0200

    Completed exercise A - list of names

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,18 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+
+  for (let person of arrayOfPeople) {
+    let h1 = document.createElement("h1");
+    let text = document.createTextNode(person.name);
+    h1.appendChild(text);
+
+    let h2 = document.createElement("h2");
+    let text2 = document.createTextNode(person.job);
+    h2.appendChild(text2);
+
+    content.appendChild(h1);
+    content.appendChild(h2);
+  }
 }
 
 let people = [
