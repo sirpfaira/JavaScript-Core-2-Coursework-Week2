e761599412819e98f4f06b4c3f04dfb6ccd2ad38
Simba Pfaira <spfaira@yahoo.com>
2021-03-05 00:18:55 +0200

    Completed exercise D - to do list

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,32 @@
 function todoList(todos) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let unorderedList = document.createElement("ul");
+
+  for (let todo of todos) {
+    let listItem = document.createElement("li");
+    let itemText = document.createTextNode(todo.todo);
+    listItem.appendChild(itemText);
+    listItem.addEventListener(
+      "click",
+      (strikeThroughText = function () {
+        let currentState = listItem.style.textDecoration;
+        if (currentState == "line-through")
+          listItem.style.textDecoration = "none";
+        else listItem.style.textDecoration = "line-through";
+      })
+    );
+    unorderedList.appendChild(listItem);
+  }
+
+  content.appendChild(unorderedList);
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
