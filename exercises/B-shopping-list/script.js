e1a02c7c43838d91f96062c7016cd4e19aa41867
Simba Pfaira <spfaira@yahoo.com>
2021-03-04 20:36:16 +0200

    Completed exercise B - shopping list

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,15 @@
-function shoppingList(arrayOfPeople) {
+function shoppingList(groceries) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let unorderedList = document.createElement("ul");
+
+  for (let grocery of groceries){
+    let listItem = document.createElement("li");
+    let itemText = document.createTextNode(grocery);
+    listItem.appendChild(itemText);
+    unorderedList.appendChild(listItem);
+  }
+  content.appendChild(unorderedList);
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
