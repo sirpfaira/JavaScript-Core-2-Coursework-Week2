79c4546d507bb5848151c04a5eeb2b84794e4dc9
Simba Pfaira <spfaira@yahoo.com>
2021-03-05 01:15:36 +0200

    Completed E - list of colors

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,22 @@
 function listOfColours(colours) {
   // Write your code here...
+  let select = document.createElement("select");
+  let content = document.getElementById("content");
+  let paragraph = document.createElement("p");
+
+  colours.forEach((color) => {
+    let option = document.createElement("option");
+    option.textContent = color;
+    select.appendChild(option);
+
+    select.addEventListener("change", function () {
+      paragraph.innerHTML = "You have selected: " + select.value;
+      paragraph.style.color = select.value;
+    });
+  });
+
+  content.appendChild(select);
+  content.appendChild(paragraph);
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
